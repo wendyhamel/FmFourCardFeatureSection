4c1803d45445fe525605f1d60f0a39284d6c4215
wendyhamel <whamel@it4web.nl>
2023-05-31 13:20:38 +0200

    Mobile styling

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -12,6 +12,12 @@ tailwind.config = {
 				'blue-dark': 'hsl(234, 12%, 34%)',
 				'blue-gray': 'hsl(229, 6%, 66%)',
 				'gray-light': 'hsl(0, 0%, 98%)',
+			},
+			fontSize: {
+				'small': '0.9375rem'
+			},
+			boxShadow: {
+				'around': '0 20px 25px 2px rgb(0 0 0 / 0.1), 0 8px 10px 3px rgb(0 0 0 / 0.1)'
 			}
 		}
 	}
