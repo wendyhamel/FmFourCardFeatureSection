d28573fc3366bd9440f53720bee7613e8248b6ff
wendyhamel <whamel@it4web.nl>
2023-05-31 11:50:12 +0200

    Added tailwind and setup basic styles.

---
+++ src/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Poppins', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				'red': 'hsl(0, 78%, 62%)',
+				'cyan': 'hsl(180, 62%, 55%)',
+				'orange': 'hsl(34, 97%, 64%)',
+				'blue': 'hsl(212, 86%, 64%)',
+				'blue-dark': 'hsl(234, 12%, 34%)',
+				'blue-gray': 'hsl(229, 6%, 66%)',
+				'gray-light': 'hsl(0, 0%, 98%)',
+			}
+		}
+	}
+}
